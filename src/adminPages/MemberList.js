8f47572e84b3b7961b6bdc4a8fa8472bf0b24c82
josiah-0208 <beth_shan@naver.com>
2022-12-05 11:05:21 +0900

    12.05

--- src/adminPages/MemberList.js
+++ src/adminPages/MemberList.js
@@ -22,18 +22,61 @@ function MemberList() {
                     <div className="memberListTitle">
                         멤버 목록
                     </div>
+                    <div className="memberListSearchArea">
+                        <div>
+                            <select>
+                                <option>이름</option>
+                                <option>아이디</option>
+                                <option>전화번호</option>
+                                <option>주소</option>
+                            </select>
+                        </div>
+                        <div>
+                            <input />
+                        </div>
+                        <div>
+                            <button>검색</button>
+                        </div>
+                    </div>
                 </div>
                 <div className="memberListMiddle">
-                    <div>
-                        {
-                            memberList.map((member, idx) => {
-                                return (
-                                    <MemberListDetail member={member}
-                                        key={"memberListDetail" + idx} />
-                                )
-                            })
-                        }
-                    </div>
+                    <table className="memberListTable">
+                        <thead>
+                            <tr>
+                                <th>
+                                    ID
+                                </th>
+                                <th>
+                                    이름
+                                </th>
+                                <th>
+                                    전화번호
+                                </th>
+                                <th>
+                                    주소
+                                </th>
+                                <th>
+                                    가입일
+                                </th>
+                                <th>
+                                    탈퇴여부
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                memberList.map((member, idx) => {
+                                    return (
+                                        <MemberListDetail member={member}
+                                            key={"memberListDetail" + idx} />
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                <div className="memberListBottom">
+                                
                 </div>
             </div>
         </div>
@@ -44,27 +87,37 @@ export default MemberList;
 
 function MemberListDetail(props) {
     return (
-        <div className="conatiner_memberlistdetail">
-            <div className="memberListDetailBox">
+        <tr>
+            <td>
                 <div className="memberListDetailId">
                     {props.member.id}
                 </div>
+            </td>
+            <td>
                 <div className="memberListDetailName">
                     {props.member.name}
                 </div>
+            </td>
+            <td>
                 <div className="memberListDetailTel">
                     {props.member.tel}
                 </div>
+            </td>
+            <td>
                 <div className="memberListDetailAddress">
                     {props.member.address1}
                 </div>
+            </td>
+            <td>
                 <div className="memberListDetailJoinDate">
                     {props.member.joinDate}
                 </div>
+            </td>
+            <td>
                 <div className="memberListDetailDel">
                     {props.member.del}
                 </div>
-            </div>
-        </div>
+            </td>
+        </tr>
     )
 }
